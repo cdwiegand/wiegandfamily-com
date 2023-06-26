d3ec2eca2a9429dae8a4dcdab23adf383920eb09
Chris Wiegand <chris@wiegandtech.net>
2023-06-25 21:41:34 -0600

    add groups!

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Build process
 
 1. `npx tailwindcss -i ./site.css -o ./tailwind.css -m`
+
+# Local run process
+
+1. `npx tailwindcss -i ./site.css -o ./tailwind.css -m -w &`
+2. `python3 -m http.server`
--- site.js
+++ site.js
@@ -9,23 +9,38 @@ function wiegand_page_init() {
   }
 
   const bgs = ["black", "purple", "red", "orange", "green", "white", "techelet"];
-  const templateDiv = document.getElementById("template_div");
-  wiegand_page_links.forEach((val, idx) => {
-    let newOneHtml = templateDiv.innerHTML;
-    let defaultBackground = "bg-" + bgs[Math.floor(Math.random() * bgs.length)];
-    newOneHtml = newOneHtml.replace('id="template_div"', 'id="item_' + idx + '"');
-    newOneHtml = newOneHtml.replace("{{href}}", val.href);
-    newOneHtml = newOneHtml.replace("{{text}}", val.text);
-    newOneHtml = newOneHtml.replace("{{icon}}", val.icon ?? "");
-    newOneHtml = newOneHtml.replace("{{alt}}", val.alt ?? "");
-    newOneHtml = newOneHtml.replace("{{bgcolor}}", val.bgcolor ?? defaultBackground);
-    newOneHtml = newOneHtml.replace("{{description}}", val.description ?? "");
-    newOneHtml = newOneHtml.replace(" template-", " ");
-    let newOne = document.createElement("div");
-    newOne.innerHTML = newOneHtml;
-    templateDiv.parentNode.insertBefore(newOne, templateDiv);
-    newOne.setAttribute("display", "inherit");
+  const groupTemplateDiv = document.getElementById("group_template_div");
+  groupTemplateDiv.style.display = "none";
+  const itemTemplateDiv = document.getElementById("item_template_div");
+  itemTemplateDiv.style.display = "none";
+
+  wiegand_page_links.forEach((group, gidx) => {
+    let newGroupHtml = groupTemplateDiv.innerHTML;
+    newGroupHtml = newGroupHtml.replace(' id="group_template_div"', "");
+    newGroupHtml = newGroupHtml.replace("{{title}}", group.title);
+    let newGroup = document.createElement("div");
+    newGroup.innerHTML = newGroupHtml;
+    newGroup.id = "group_" + gidx;
+    groupTemplateDiv.parentNode.appendChild(newGroup);
+    newGroup.style.display = "inherit";
+
+    group.links.forEach((val, idx) => {
+      let newOneHtml = itemTemplateDiv.innerHTML;
+      let defaultBackground = "bg-" + bgs[Math.floor(Math.random() * bgs.length)];
+      newOneHtml = newOneHtml.replace(' id="item_template_div"', "");
+      newOneHtml = newOneHtml.replace("{{href}}", val.href);
+      newOneHtml = newOneHtml.replace("{{text}}", val.text);
+      newOneHtml = newOneHtml.replace("{{icon}}", val.icon ?? "");
+      newOneHtml = newOneHtml.replace("{{alt}}", val.alt ?? "");
+      newOneHtml = newOneHtml.replace("{{bgcolor}}", val.bgcolor ?? defaultBackground);
+      newOneHtml = newOneHtml.replace("{{description}}", val.description ?? "");
+      newOneHtml = newOneHtml.replace(" template-", " ");
+      let newOne = document.createElement("div");
+      newOne.innerHTML = newOneHtml;
+      newOne.id = "item_" + gidx + "_" + idx;
+      newGroup.appendChild(newOne);
+      newOne.style.display = "inherit";
+    });
   });
-  templateDiv.style.display = "none";
 }
 wiegand_page_init();
